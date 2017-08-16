dd8793763462af1b9e8a048713908d9f22c83faf
bigballon <605718199@qq.com>
2017-08-16 16:20:04 +0800

    commit #13: fixed bug

--- 6_ResNeXt/ResNeXt_keras.py
+++ 6_ResNeXt/ResNeXt_keras.py
@@ -1,9 +1,10 @@
 import keras
+import math
 import numpy as np
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.normalization import BatchNormalization
-from keras.layers import Conv2D, Dense, Input, add, Activation, LeakyReLU, GlobalAveragePooling2D
+from keras.layers import Conv2D, Dense, Input, add, Activation, GlobalAveragePooling2D
 from keras.layers import Lambda, concatenate
 from keras.initializers import he_normal
 from keras.callbacks import LearningRateScheduler, TensorBoard
@@ -11,15 +12,18 @@ from keras.models import Model
 from keras import optimizers
 from keras import regularizers
 
-cardinality        = 16
+cardinality        = 8
+base_width         = 64
+inplanes           = 64
+expansion          = 4
 
-num_classes        = 10
 img_rows, img_cols = 32, 32
 img_channels       = 3
+num_classes        = 10
 batch_size         = 128
 epochs             = 200
 iterations         = 391
-weight_decay       = 0.0001
+weight_decay       = 0.0002
 
 mean = [125.307, 122.95, 113.865]
 std  = [62.9932, 62.0887, 66.7048]
@@ -29,52 +33,54 @@ def scheduler(epoch):
     return lr[epoch // 55]
 
 def resnext(img_input,classes_num):
+    global inplanes
     def add_common_layer(x):
         x = BatchNormalization()(x)
-        x = LeakyReLU()(x)
+        x = Activation('relu')(x)
         return x
 
-    def group_conv(x,out_filters,strides):
-        if cardinality == 1:
-            return Conv2D(out_filters,kernel_size=(3,3),strides=strides,padding='same')(x)
-        
-        h = out_filters // cardinality
+    def group_conv(x,planes,stride):
+        h = planes // cardinality
         groups = []
         for i in range(cardinality):
             group = Lambda(lambda z: z[:,:,:, i * h : i * h + h])(x)
-            groups.append(Conv2D(h,kernel_size=(3,3),strides=strides,padding='same')(group))
+            groups.append(Conv2D(h,kernel_size=(3,3),strides=stride,kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),padding='same',use_bias=False)(group))
         x = concatenate(groups)
         return x
 
-    def residual_block(x,in_filters,out_filters,increase_filter=False):
-        first_stride = (1,1)
-        if increase_filter:
-            first_stride = (2,2)
+    def residual_block(x,planes,stride=(1,1)):
+
+        D = int(math.floor(planes * (base_width/64.0)))
+        C = cardinality
+
         shortcut = x
-        y = add_common_layer(shortcut)
         
-        y = Conv2D(in_filters,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(y)
+        y = Conv2D(D*C,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(shortcut)
         y = add_common_layer(y)
 
-        y = group_conv(y,in_filters,strides=first_stride)
+        y = group_conv(y,D*C,stride)
         y = add_common_layer(y)
 
-        y = Conv2D(out_filters, kernel_size=(1,1), strides=(1,1), padding='same', kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(y)
+        y = Conv2D(planes*expansion, kernel_size=(1,1), strides=(1,1), padding='same', kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(y)
+        y = add_common_layer(y)
 
-        if increase_filter or in_filters != out_filters:
-            shortcut = Conv2D(out_filters,kernel_size=(1,1),strides=first_stride,padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(shortcut)
-        
+        if stride != (1,1) or inplanes != planes * expansion:
+            shortcut = Conv2D(planes * expansion, kernel_size=(1,1), strides=stride, padding='same', kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+            shortcut = BatchNormalization()(shortcut)
         y = add([y,shortcut])
+        y = Activation('relu')(y)
         return y
     
-    def residual_layer(x,num,in_filters,out_filters,increase_filter=False):
-        x = residual_block(x,in_filters,out_filters,increase_filter)
-        for _ in range(1,num):
-            x = residual_block(x,in_filters,out_filters)
-        return x        
+    def residual_layer(x, blocks, planes, stride=(1,1)):
+        x = residual_block(x, planes, stride)
+        inplanes = planes * expansion
+        for i in range(1,blocks):
+            x = residual_block(x,planes)
+        return x
     
     def conv3x3(x,filters):
-        return Conv2D(filters=filters, kernel_size=(3,3), strides=(1,1), padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(x)
+        x = Conv2D(filters=filters, kernel_size=(3,3), strides=(1,1), padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+        return add_common_layer(x)
 
     def dense_layer(x):
         return Dense(classes_num,activation='softmax',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(x)
@@ -82,11 +88,9 @@ def resnext(img_input,classes_num):
 
     # build the resnext model    
     x = conv3x3(img_input,64)
-    x = residual_layer(x, 3, 64, 128)
-    x = residual_layer(x, 3, 128, 256,increase_filter=True)
-    x = residual_layer(x, 3, 256, 512,increase_filter=True)
-    x = BatchNormalization()(x)
-    x = LeakyReLU()(x)
+    x = residual_layer(x, 3, 64)
+    x = residual_layer(x, 3, 128,stride=(2,2))
+    x = residual_layer(x, 3, 256,stride=(2,2))
     x = GlobalAveragePooling2D()(x)
     x = dense_layer(x)
     return x
