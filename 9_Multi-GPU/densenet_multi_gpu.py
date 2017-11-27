d0cd2f443af1dbb5a3a09dac071aa8bc25cfa3d5
bigballon <605718199@qq.com>
2017-11-28 03:50:41 +0800

    commit #32: add multi-gpu example

---
+++ 9_Multi-GPU/densenet_multi_gpu.py
@@ -0,0 +1,183 @@
+import keras
+import math
+import numpy as np
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Conv2D, Dense, Input, add, Activation, AveragePooling2D, GlobalAveragePooling2D
+from keras.layers import Lambda, concatenate
+from keras.initializers import he_normal
+from keras.layers.merge import Concatenate
+from keras.callbacks import LearningRateScheduler, TensorBoard, ModelCheckpoint
+from keras.models import Model
+from keras import optimizers
+from keras import regularizers
+from keras.utils import plot_model
+
+import tensorflow as tf
+from keras.models import *
+from keras.layers import merge, Lambda
+from keras import backend as K
+
+
+# change the following para according to your GPUs.
+gpu_number         = 2
+batch_size         = 64         # 64 or 32 or smaller
+epochs             = 250        
+iterations         = 782       
+
+growth_rate        = 24 
+depth              = 164
+compression        = 0.5
+
+img_rows, img_cols = 32, 32
+img_channels       = 3
+num_classes        = 10
+weight_decay       = 0.0001
+
+mean = [125.307, 122.95, 113.865]
+std  = [62.9932, 62.0887, 66.7048]
+
+session_config = tf.ConfigProto()
+session_config.gpu_options.allow_growth = True
+session = tf.Session(config=session_config)
+
+def slice_batch(x, n_gpus, part):
+    sh = K.shape(x)
+    L =  sh[0] // n_gpus
+    if part == n_gpus - 1:
+        return x[part*L:]
+    return x[part*L:(part+1)*L]
+
+
+def to_multi_gpu(model, n_gpus=2):
+    if n_gpus ==1:
+        return model
+    
+    with tf.device('/cpu:0'):
+        x = Input(model.input_shape[1:])
+    towers = []
+    for g in range(n_gpus):
+        with tf.device('/gpu:' + str(g)):
+            slice_g = Lambda(slice_batch, lambda shape: shape, arguments={'n_gpus':n_gpus, 'part':g})(x)
+            towers.append(model(slice_g))
+
+    with tf.device('/cpu:0'):
+        merged = Concatenate(axis=0)(towers)
+    return Model(inputs=[x], outputs=merged)
+
+def scheduler(epoch):
+    if epoch <= 75:
+        return 0.1
+    if epoch <= 150:
+        return 0.01
+    if epoch <= 210:
+        return 0.001
+    return 0.0005
+
+def densenet(img_input,classes_num):
+
+    def bn_relu(x):
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        return x
+
+    def bottleneck(x):
+        channels = growth_rate * 4
+        x = bn_relu(x)
+        x = Conv2D(channels,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+        x = bn_relu(x)
+        x = Conv2D(growth_rate,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+        return x
+
+    def single(x):
+        x = bn_relu(x)
+        x = Conv2D(growth_rate,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+        return x
+
+    def transition(x, inchannels):
+        outchannels = int(inchannels * compression)
+        x = bn_relu(x)
+        x = Conv2D(outchannels,kernel_size=(1,1),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(x)
+        x = AveragePooling2D((2,2), strides=(2, 2))(x)
+        return x, outchannels
+
+    def dense_block(x,blocks,nchannels):
+        concat = x
+        for i in range(blocks):
+            x = bottleneck(concat)
+            concat = concatenate([x,concat], axis=-1)
+            nchannels += growth_rate
+        return concat, nchannels
+
+    def dense_layer(x):
+        return Dense(classes_num,activation='softmax',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay))(x)
+
+
+    nblocks = (depth - 4) // 6 
+    nchannels = growth_rate * 2
+
+    x = Conv2D(nchannels,kernel_size=(3,3),strides=(1,1),padding='same',kernel_initializer=he_normal(),kernel_regularizer=regularizers.l2(weight_decay),use_bias=False)(img_input)
+
+    x, nchannels = dense_block(x,nblocks,nchannels)
+    x, nchannels = transition(x,nchannels)
+    x, nchannels = dense_block(x,nblocks,nchannels)
+    x, nchannels = transition(x,nchannels)
+    x, nchannels = dense_block(x,nblocks,nchannels)
+    x, nchannels = transition(x,nchannels)
+    x = bn_relu(x)
+    x = GlobalAveragePooling2D()(x)
+    x = dense_layer(x)
+    return x
+
+
+if __name__ == '__main__':
+
+    # load data
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test  = keras.utils.to_categorical(y_test, num_classes)
+    x_train = x_train.astype('float32')
+    x_test  = x_test.astype('float32')
+    
+    # - mean / std
+    for i in range(3):
+        x_train[:,:,:,i] = (x_train[:,:,:,i] - mean[i]) / std[i]
+        x_test[:,:,:,i] = (x_test[:,:,:,i] - mean[i]) / std[i]
+
+    # build network
+    img_input = Input(shape=(img_rows,img_cols,img_channels))
+    output    = densenet(img_input,num_classes)
+    model     = Model(img_input, output)
+
+    
+    # -------------- Multi-GPU-------------------#
+    model     = to_multi_gpu(model,n_gpus=gpu_number) 
+    # -------------- Multi-GPU-------------------#
+
+    # model.load_weights('ckpt.h5')
+    
+    # https://github.com/fchollet/keras/issues/3210
+    # plot_model(model, show_shapes=True, to_file='model.png')
+    
+    print(model.summary())
+
+    # set optimizer
+    sgd = optimizers.SGD(lr=.1, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+    # set callback
+    tb_cb     = TensorBoard(log_dir='./densenet/', histogram_freq=0)
+    change_lr = LearningRateScheduler(scheduler)
+    ckpt      = ModelCheckpoint('./ckpt.h5', save_best_only=False, mode='auto', period=10)
+    cbks      = [change_lr,tb_cb,ckpt]
+
+    # set data augmentation
+    print('Using real-time data augmentation.')
+    datagen   = ImageDataGenerator(horizontal_flip=True,width_shift_range=0.125,height_shift_range=0.125,fill_mode='constant',cval=0.)
+
+    datagen.fit(x_train)
+
+    # start training
+    model.fit_generator(datagen.flow(x_train, y_train,batch_size=batch_size), steps_per_epoch=iterations, epochs=epochs, callbacks=cbks,validation_data=(x_test, y_test))
+    model.save('densenet.h5')
